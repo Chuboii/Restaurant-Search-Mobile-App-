f25eee3f85a0813adf28563a829194512fd859d8
Chuboii <favoronyechere@gmail.com>
2024-04-18 17:37:30 +0100

    commit

--- utils/render screens/RenderScreens.tsx
+++ utils/render screens/RenderScreens.tsx
@@ -8,23 +8,10 @@ import TabNavigation from "../../utils/tab navigation/TabNavigation"
 import SettingsScreen from "../../screens/settings/SettingsScreen"
 import BusinessScreen from "../../screens/business/BusinessScreen"
 import LoadScreen from "../../screens/load/LoadScreen"
-import { useEffect, useState } from "react"
-
 
 const Stack = createNativeStackNavigator()
 
 const RenderScreens = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulating some loading time (e.g., fetching data, initializing resources)
-    const timer = setTimeout(() => {
-      setLoading(false); // Set loading to false after some time (e.g., 2 seconds)
-    }, 3000); // Adjust the time as needed
-
-    return () => clearTimeout(timer); // Cleanup on unmount
-  }, [loading]);
-
 
   return (
       <NavigationContainer>
@@ -32,14 +19,9 @@ const RenderScreens = () => {
               headerShown:false
           }}
       >
-
-{loading ? (
-          // Render the loading screen while loading is true
           <Stack.Screen name="load" component={LoadScreen} />
-        ) : (
-          // Render the TabNavigation screen once loading is complete
           <Stack.Screen name="TabNavigation" component={TabNavigation} />
-        )}
+ 
               <Stack.Screen name="home" component={HomeScreen} />
               <Stack.Screen name="search" component={SearchScreen} />
              <Stack.Screen name="error" component={ErrorScreen} />
